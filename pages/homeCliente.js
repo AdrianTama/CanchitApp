0b404d2d860964ce92cc85ec89565dc021fed707
adriantama <tokudatamashiro@gmail.com>
2020-11-21 19:49:30 -0300

    vista mi reserva

--- pages/homeCliente.js
+++ pages/homeCliente.js
@@ -9,15 +9,52 @@ import s from '../components/styles'
 
 export default function Home() {
 
+    const ip = 'https://secret-shore-39623.herokuapp.com/';
     const navigation = useNavigation();
     const context = useContext(GlobalContext);
+    const [response, setResponse] = useState("");
+
+    useEffect(() => {
+
+        setResponse(buscarReserva()) 
+
+    }, [])
+
+    async function buscarReserva() {
+        const headers = new Headers();
+
+        headers.append("Content-type", "application/json")
+
+        const requestOptions = {
+            method: "GET",
+            headers: headers
+        }
+
+        let respuesta = await fetch(ip + 'api/reservas/miReserva/' + context.usuario.email, requestOptions)
+            .then((res) => res.json())
+            .catch(err => {
+                console.log("Error: ", err)
+            })
+            
+        return respuesta
+    }
+
+    function chequeoReserva(response) {
+        console.log(response)
+        if (response != false) {
+            navigation.navigate("Mi Reserva")
+        } else {
+            navigation.navigate("Nueva Reserva")
+        }
+    }
+
 
     return (
         <ScrollView style={s.container} >
             <BotoneraSuperior />
 
             <View style={s.botonera}>
-                <TouchableHighlight style={s.containerBotonHome} onPress={() => navigation.navigate("Nueva Reserva")}>
+                <TouchableHighlight style={s.containerBotonHome} onPress={() => chequeoReserva(response)}>
                     <View style={s.boton}>
                         <Text style={s.textoBoton}>Reservar Cancha</Text>
                     </View>
@@ -32,6 +69,6 @@ export default function Home() {
 
             </View>
 
-        </ScrollView>
+        </ScrollView >
     );
 }
--- pages/mireserva.js
+++ pages/mireserva.js
@@ -1,16 +1,61 @@
-import React from 'react';
-import { Picker, StyleSheet, Text, View, TouchableHighlight, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect, useContext } from 'react';
+import { Picker, StyleSheet, Text, View, TouchableHighlight, TouchableOpacity, Alert } from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
 
 import s from '../components/styles';
 import BotoneraSuperior from '../components/botoneraSuperior';
 import { ScrollView } from 'react-native-gesture-handler';
 
-export default function MiReserva() {
+export default function MiReserva({ route }) {
+    const { reserva } = route.params
+    const context = useContext(GlobalContext);
+    const [tipoElegido] = useState(context.reserva.tipoElegido)
+    const [canchaElegida] = useState(context.reserva.canchaElegida)
+    const [diaElegido] = useState(context.reserva.diaElegido)
+    const [horarioElegido] = useState(context.reserva.horarioElegido)
 
+    const [puedeEnviar, setPuedeEnviar] = useState(false)
     const navigation = useNavigation();
 
+    useEffect(() => {
 
+        setPuedeEnviar(tipoElegido != 0 && canchaElegida != '' && diaElegido != '' && horarioElegido != '')
+
+    }, [tipoElegido, canchaElegida, diaElegido, horarioElegido])
+
+    async function guardarUsuario() {
+
+        if (puedeEnviar) {
+
+            //Conformación de componentes para el fetch
+            const headers = new Headers();
+
+            headers.append("Content-type", "application/json")
+
+            const requestOptions = {
+                method: "GET",
+                headers: headers,
+                body: JSON.stringify({
+                    tipoElegido: tipoElegido,
+                    canchaElegida: canchaElegida,
+                    diaElegido: diaElegido,
+                    horarioElegido: horarioElegido
+                })
+            }
+
+            //Almacenamos el response del fetch
+            let response = await fetch(ip + 'api/reservas/miReserva' + context.usuario.email, requestOptions)
+                .then((res) => res.json())
+                .catch(err => {
+                    console.log("Error: ", err)
+                })
+            //Dependiendo el response, mostramos un msj    
+            if (reserva == false) {
+                Alert.alert("No se registra ninguna reserva")
+            }
+
+        }
+    }
 
     return (
         <ScrollView style={s.container} >
@@ -18,9 +63,9 @@ export default function MiReserva() {
             <Text style={s.subtitulo}>Mi reserva</Text>
 
             <View style={s.reserva}>
-                <Text style={s.texto}>Cancha: </Text>
-                <Text style={s.texto}>Dia: </Text>
-                <Text style={s.texto}>Hora: </Text>
+                <Text style={s.texto}>Cancha: {reserva.nroCancha}</Text>
+                <Text style={s.texto}>Dia: {reserva.dia}</Text>
+                <Text style={s.texto}>Hora: {reserva.hora}</Text>
             </View>
 
             <TouchableHighlight style={s.containerBoton} onPress={() => navigation.navigate('Home')}>
